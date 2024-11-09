d4e10d9927b66c1eecb07f1ac8245d709a741add
Louis Ward <louis.ward@ocgsoftware.com>
2024-11-09 04:07:06 +0000

    tidy

--- index.js
+++ index.js
@@ -11,29 +11,24 @@ const client = new Client({
   ],
 });
 
-// When the client is ready, run this code (only once).
-// The distinction between `client: Client<boolean>` and `readyClient: Client<true>` is important for TypeScript developers.
-// It makes some properties non-nullable.
-client.once(Events.ClientReady, (readyClient) => {
-  console.log(`Ready! Logged in as ${readyClient.user.tag}`);
+// Event
+// Boot up ChowBot
+client.once(Events.ClientReady, (ready) => {
+  console.log(`${ready.user.tag} INITIALISED`);
 });
 
-// MessageCreate
+// Event
+// Reddit React to Image/Video. !Only checks first attachment!
 client.on(Events.MessageCreate, (messageCreate) => {
-  console.log("MessageCreate");
-  //console.log(messageCreate);
-  if (messageCreate.attachments.size > 0) {
-    const firstContentType = messageCreate.attachments.first().contentType;
-    console.log("contentType: " + firstContentType);
-    if (null == firstContentType) return;
-    if (
-      firstContentType.includes("image") ||
-      firstContentType.includes("video")
-    ) {
-      console.log("MessageCreate - has image must react");
-      messageCreate.react("<:blobreach:1304553163512352788>");
-      messageCreate.react("<:blobreach:1304553174509817957>");
-    }
+  console.log("redditReact - heard");
+  if (messageCreate.attachments.size == 0) return;
+  if (null == contentType) return;
+  if (contentType.includes("image") || contentType.includes("video")) {
+    console.log("redditReact - content detected must react");
+    messageCreate
+      .react("1304553163512352788")
+      .then(() => messageCreate.react("1304553174509817957"))
+      .catch((error) => console.error("redditReact - failed to react :(", error));
   }
 });
 
