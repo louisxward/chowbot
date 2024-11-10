f4b9f887cf2453e705341d1318082217bc6beb30
Louis Ward <louis.ward@ocgsoftware.com>
2024-11-10 18:31:23 +0000

    react to embed message

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -5,14 +5,15 @@ module.exports = {
   name: Events.MessageCreate,
   execute(message) {
     console.log("[INFO] MessageCreate");
-    if (message.attachments.size == 0) return;
-    const contentType = message.attachments.first().contentType;
-    if (null == contentType) return;
-    if (contentType.includes("image") || contentType.includes("video")) {
-      message
-        .react(EMOJI_UPVOTE_ID)
-        .then(() => message.react(EMOJI_DOWNVOTE_ID))
-        .catch((error) => console.error(error));
+    if (message.embeds.length == 0) {
+      if (message.attachments.size == 0) return;
+      const contentType = message.attachments.first().contentType;
+      if (null == contentType) return;
+      if (!contentType.includes("image") && !contentType.includes("video")) return;
     }
+    message
+      .react(EMOJI_UPVOTE_ID)
+      .then(() => message.react(EMOJI_DOWNVOTE_ID))
+      .catch((error) => console.error(error));
   },
 };
