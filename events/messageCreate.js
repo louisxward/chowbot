7f4b42cd0ac7130d2a7255486af6a1005e434d53
Louis Ward <louis.ward@ocgsoftware.com>
2024-11-11 22:54:30 +0000

    fetch message

--- events/messageCreate.js
+++ events/messageCreate.js
@@ -3,8 +3,16 @@ const { EMOJI_UPVOTE_ID, EMOJI_DOWNVOTE_ID } = require("../constants.js");
 
 module.exports = {
   name: Events.MessageCreate,
-  execute(message) {
+  async execute(message) {
     console.log("[INFO] MessageCreate");
+    if (message.partial) {
+      try {
+        await message.fetch();
+      } catch (error) {
+        console.error(error);
+        return;
+      }
+    }
     if (message.embeds.length == 0) {
       if (message.attachments.size == 0) return;
       const contentType = message.attachments.first().contentType;
