254974437b01b652037730dd51c9a52b51099790
Louis Ward <louis.ward@ocgsoftware.com>
2024-11-17 16:37:08 +0000

    command

--- commands/reddit/react.js
+++ commands/reddit/react.js
@@ -12,31 +12,20 @@ module.exports = {
   async execute(interaction) {
     logger.info("command - react");
     logger.info(`- userId: ${interaction.user.id}`);
-    const inputMessageId = interaction.options.getString("message_id") ?? "";
-    if (null == inputMessageId || inputMessageId.length == 0) {
+    const inputMessageId = interaction.options.getString("message_id");
+    if (!inputMessageId) {
       await interaction.reply({ content: "message_id is empty", ephemeral: true });
       return;
     }
     logger.info(`- inputMessageId: ${inputMessageId}`);
-    if (interaction.channel) {
-      const channel = interaction.channel;
-      let message;
-      try {
-        message = await channel.messages.fetch(inputMessageId);
-      } catch (error) {
-        logger.warn(error);
-        await interaction.reply({ content: "message_id is invalid", ephemeral: true });
-        return;
-      }
-      logger.info(`- foundMessageId: ${message.id}`);
-      try {
-        await message.react(EMOJI_UPVOTE_ID).then(() => message.react(EMOJI_DOWNVOTE_ID));
-      } catch (error) {
-        logger.error(error);
-        await interaction.reply({ content: "didnt work, broken :/", ephemeral: true });
-        return;
-      }
+    try {
+      const message = await channel.messages.fetch(inputMessageId);
+      logger.info("- found: true");
+      await message.react(EMOJI_UPVOTE_ID).then(() => message.react(EMOJI_DOWNVOTE_ID));
       await interaction.reply({ content: "reacted :P", ephemeral: true });
+    } catch (error) {
+      logger.error(error);
+      await interaction.reply({ content: "message_id is invalid", ephemeral: true });
     }
   }
 };
