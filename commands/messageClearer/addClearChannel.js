3f2ec26e3a3f0394cb8d4a35b7681b5355653d9d
Louis Ward <louis.ward@ocgsoftware.com>
2025-05-25 22:18:30 +0100

    rename perms

--- commands/messageClearer/addClearChannel.js
+++ commands/messageClearer/addClearChannel.js
@@ -4,23 +4,17 @@ const { addServerClearChannel } = require("services/messageClearer");
 
 module.exports = {
   data: new SlashCommandBuilder()
-    .setName("addchannel")
-    .setDescription("addchannel - !!!DANGEROUS!!! add channel to clear messages from at 05:00 UTC Daily")
-    .addStringOption((option) => option.setName("channel_id").setDescription("id of the channel").setRequired(true))
+    .setName("addClearChannel")
+    .setDescription("addClearChannel - !!!DANGEROUS!!! add channel to clear messages from at 05:00 UTC Daily")
+    .addStringOption((option) => option.setName("channel_id").setRequired(true))
     .setDefaultMemberPermissions(PermissionFlagsBits.Administrator),
   async execute(interaction) {
-    logger.info("command - addchannel");
+    logger.info("command - addClearChannel");
     logger.info(`- userId: ${interaction.user.id}`);
     const serverId = interaction.guildId;
     logger.info(`- serverId: ${serverId}`);
     const channelId = interaction.options.getString("channel_id");
     logger.info(`- channelId: ${channelId}`);
-    // if (await addServerClearChannel(interaction.client, serverId, channelId)) {
-    //   await interaction.reply({ content: "channel_id has been added", ephemeral: true });
-    // } else {
-    //   await interaction.reply({ content: "channel_id is already added", ephemeral: true });
-    // }
-
     try {
       await addServerClearChannel(interaction.client, serverId, channelId);
       await interaction.reply({ content: "channel_id has been added", ephemeral: true });
