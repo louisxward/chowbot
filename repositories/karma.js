2492fae7541c6ed8ab7628455d309f7bfb95d032
Louis Ward <louis.ward@ocgsoftware.com>
2025-05-27 20:39:34 +0100

    remove message

--- repositories/karma.js
+++ repositories/karma.js
@@ -2,4 +2,6 @@ const logger = require("logger");
 const sqlite3 = require("sqlite3").verbose();
 const { connect } = require("services/sqlInitService");
 
+async function 
+
 module.exports = { updateUserKarma, getUserKarma, getKarmaLeaderboard };
--- services/sqlInitService.js
+++ services/sqlInitService.js
@@ -7,28 +7,24 @@ async function init() {
   const db = await connect();
   // Table Create
   await db.exec(`
-    CREATE TABLE IF NOT EXISTS Servers (
+    CREATE TABLE IF NOT EXISTS Server (
       id INTEGER PRIMARY KEY,
       name TEXT NOT NULL
     );
     `);
   await db.exec(`
-    CREATE TABLE IF NOT EXISTS Message (
-      id INTEGER PRIMARY KEY,
-      serverId INTEGER NOT NULL,
-      userId INTEGER NOT NULL,
-      messageId INTEGER NOT NULL
-    );
-    `);
-  await db.exec(`
     CREATE TABLE IF NOT EXISTS Karma (
-      id INTEGER PRIMARY KEY,
-      userId INTEGER NOT NULL,
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      serverId INTEGER NOT NULL,
       messageId INTEGER NOT NULL,
+      messageUserId INTEGER NOT NULL,
+      reactionId INTEGER NOT NULL,
+      reactionUserId INTEGER NOT NULL,
       value INTEGER NOT NULL
     );
     `);
   // Dev Data
+  // todo - change this to auto add servers on startup or on bot inv
   try {
     await db.run("INSERT INTO Servers (id, name) VALUES (?, ?)", ["1309582350849544304", "chowbotdevtalosclone"]);
   } catch (error) {
