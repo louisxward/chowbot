5760b0782b2597c49eb8ddb8e780ad02df6df92e
Louis Ward <louis.ward@ocgsoftware.com>
2026-01-11 03:25:02 +0000

    embed

--- commands/reddit/karma.js
+++ commands/reddit/karma.js
@@ -5,25 +5,26 @@ module.exports = {
   data: new SlashCommandBuilder().setName("karma").setDescription("Karma leaderboard"),
   async execute(interaction) {
     const leaderboard = await getKarmaLeaderboard(interaction);
+    let replyMessage = "";
     if (!leaderboard || leaderboard.size == 0) {
-      await interaction.reply({ content: "Leaderboard is empty", ephemeral: true });
-      return;
-    }
-    let replyMessage = "**Karma Leaderboard**\n";
-    let pos = 0;
-    let medal = null;
-    for (const [key, value] of leaderboard.entries()) {
-      pos += 1;
-      medal = null;
-      if (pos === 1) {
-        medal = `🥇`;
-      } else if (pos === 2) {
-        medal = `🥈`;
-      } else if (pos === 3) {
-        medal = `🥉`;
+      replyMessage = "Empty";
+    } else {
+      let pos = 0;
+      let medal = null;
+      for (const [key, value] of leaderboard.entries()) {
+        pos += 1;
+        medal = null;
+        if (pos === 1) {
+          medal = `🥇`;
+        } else if (pos === 2) {
+          medal = `🥈`;
+        } else if (pos === 3) {
+          medal = `🥉`;
+        }
+        replyMessage += `${medal ? medal : pos.toString() + ". "} ${pos < 4 ? "**" + key + "**" : key}: ${value}\n`;
       }
-      replyMessage += `${medal ? medal : pos.toString() + ". "} ${pos < 4 ? "**" + key + "**" : key}: ${value}\n`;
     }
-    await interaction.reply({ content: replyMessage.trim(), ephemeral: true });
+    const embed = new EmbedBuilder().setTitle("Karma Leaderboard").setDescription(replyMessage);
+    await interaction.reply({ ephemeral: true, embeds: [embed] });
   }
 };
