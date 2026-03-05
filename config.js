1e403d00b4d1722ae9e0609e36ccda3bd69cdcf1
Louis Ward <louis.ward@ocgsoftware.com>
2026-03-05 21:56:16 +0000

    fix

--- config.js
+++ config.js
@@ -8,5 +8,5 @@ module.exports = {
   APPLICATION_CONFIG_PATH: path.join(DATA_DIR, "applicationConfig.json"),
   SESSION_STATE_PATH: path.join(DATA_DIR, "sessionState.json"),
   INVENCHECKER_STATE_PATH: path.join(DATA_DIR, "invencheckerState.json"),
-  INVENCHECKER_API_URL: process.env.INVENCHECKER_API_URL || "http://localhost:33002"
+  INVENCHECKER_API_URL: process.env.INVENCHECKER_API_URL || "http://localhost:33001"
 };
